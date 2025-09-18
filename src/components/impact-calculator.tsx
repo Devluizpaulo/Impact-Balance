1a9284b12a523b9696445b1f10fcb8064143faa0
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-09-18 21:26:48 +0000

    em horas vamso usar o paramentor correto e ou seja a Consumo Consumo Por

--- src/components/impact-calculator.tsx
+++ src/components/impact-calculator.tsx
@@ -188,7 +188,7 @@ export default function ImpactCalculator({ onCalculate, onReset }: ImpactCalcula
             if (duration > 0) {
                 const visitorTotalHours = visitorCount * duration;
                 totalParticipantHours += visitorTotalHours;
-                ucs = Math.ceil((visitorCount * duration / 8) * calculation.perCapitaFactors.dailyUcsConsumption);
+                ucs = Math.ceil(visitorCount * duration * calculation.perCapitaFactors.hourlyUcsConsumption);
                 totalParticipantDays += (visitorCount * duration / 8);
             }
         }
