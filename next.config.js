cf2ac55b547e4c4a801737e18fa68ec0cf1c5fb3
Luiz Paulo Gonçalves Miguel de Jesus <luizao16@gmail.com>
2025-12-15 19:07:27 +0000

    Estado final local

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const withNextIntl = require('next-intl/plugin')(
 );
  
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
